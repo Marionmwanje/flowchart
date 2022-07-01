376e02ed1fd1ea67bcedb6b498113d4125a9f6f7
Marionmwanje <marionfavor15@gmail.com>
2022-07-01 08:27:27 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+let temperature=prompt("Do you want to check the weather");
+if (temperature ===`yes`){
+    console.log("Below freezing point");
+}
+else{
+    console.log("Above freezing point");
+}
+
+
+let isSnowing=prompt("is it snowing?");
+if (isSnowing ===`yes`){
+    console.log("walk to school");
+}
+else{
+    console.log("go through frosty road");
+} 
+
+
+let goThrough=prompt("go through frosty road");
+if (goThrough ===`yes`){
+    console.log("walk to school");
+}
+else{
+    console.log("cycle to school");
+    console.log("reach school");
+}
+
+
+
+let cycleTo=prompt
